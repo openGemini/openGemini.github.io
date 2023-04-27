5876bf8907c29b188dafb84e36dfad248aeacc64
shilinlee <836160610@qq.com>
2023-04-28 00:31:52 +0800

    fix: add search function
    Signed-off-by: shilinlee <836160610@qq.com>

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,24 +1,53 @@
-import { defineUserConfig } from "vuepress";
+import {defineUserConfig} from "vuepress";
+import {searchProPlugin} from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: '/docs/',
+    base: '/docs/',
 
-  locales: {
-    '/': {
-      lang: 'en-US',
-      title: 'openGemini',
-      description: 'openGemini docs site',
+    locales: {
+        '/': {
+            lang: 'en-US',
+            title: 'openGemini',
+            description: 'openGemini docs site',
+        },
+        '/zh/': {
+            lang: 'zh-CN',
+            title: 'openGemini',
+            description: 'openGemini 统一文档站点',
+        },
     },
-    '/zh/': {
-      lang: 'zh-CN',
-      title: 'openGemini',
-      description: 'openGemini 统一文档站点',
-    },
-  },
 
-  theme,
+    theme,
 
-  // Enable it with pwa
-  // shouldPrefetch: false,
+    plugins: [
+        searchProPlugin({
+            // 索引全部内容
+            indexContent: true,
+            // 为分类和标签添加索引
+            customFields: [
+//        {
+//          getter: (page) => page.frontmatter.category,
+//          formatter: "分类：$content",
+//        },
+//        {
+//          getter: (page) => page.frontmatter.tag,
+//          formatter: "标签：$content",
+//        },
+            ],
+            locales: {
+                "/": {
+                    placeholder: "search",
+                },
+
+                "/zh/": {
+                    placeholder: "开始搜索",
+                },
+            },
+        }),
+    ],
+
+
+    // Enable it with pwa
+    // shouldPrefetch: false,
 });
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,69 +1,73 @@
-import { hopeTheme } from "vuepress-theme-hope";
-import { enNavbar, zhNavbar } from "./navbar/index.js";
-import { enSidebar, zhSidebar } from "./sidebar/index.js";
+import {hopeTheme} from "vuepress-theme-hope";
+import {enNavbar, zhNavbar} from "./navbar/index.js";
+import {enSidebar, zhSidebar} from "./sidebar/index.js";
 
 export default hopeTheme({
-  hostname: "https://shilinlee.github.io/docs",
+    hostname: "https://shilinlee.github.io/docs",
 
-  author: {
-    name: "openGemini",
-    url: "http://www.opengemini.org/",
-  },
-
-  iconAssets: "iconfont",
-
-  logo: "/logo.png",
-
-  repo: "openGemini/openGemini",
-
-  docsDir: "demo/theme-docs/src",
-
-  locales: {
-    "/": {
-      // navbar
-      navbar: enNavbar,
-
-      // sidebar
-      sidebar: enSidebar,
-
-      footer: "Default footer",
-
-      displayFooter: true,
-
-      metaLocales: {
-        editLink: "Edit this page on GitHub",
-      },
+    author: {
+        name: "openGemini",
+        url: "https://github.com/openGemini/openGemini",
     },
 
-    /**
-     * Chinese locale config
-     */
-    "/zh/": {
-      // navbar
-      navbar: zhNavbar,
+    pageInfo: ['Author', 'Original', 'Date', 'Category', 'Tag', 'ReadingTime', 'Word'],
 
-      // sidebar
-      sidebar: zhSidebar,
+    iconAssets: "iconfont",
 
-      footer: "",
+    logo: "/logo.png",
 
-      displayFooter: true,
+    repo: "openGemini/docs",
 
-      // page meta
-      metaLocales: {
-        editLink: "在 GitHub 上编辑此页",
-      },
+    docsDir: "/src",
+
+    darkmode: "toggle",
+
+    locales: {
+        "/": {
+            // navbar
+            navbar: enNavbar,
+
+            // sidebar
+            sidebar: enSidebar,
+
+            footer: "Default footer",
+
+            displayFooter: true,
+
+            metaLocales: {
+                editLink: "Edit this page on GitHub",
+            },
+        },
+
+        /**
+         * Chinese locale config
+         */
+        "/zh/": {
+            // navbar
+            navbar: zhNavbar,
+
+            // sidebar
+            sidebar: zhSidebar,
+
+            footer: "",
+
+            displayFooter: true,
+
+            // page meta
+            metaLocales: {
+                editLink: "在 GitHub 上编辑此页",
+            },
+        },
     },
-  },
 
-  encrypt: {
-    config: {
-      "/demo/encrypt.html": ["1234"],
-      "/zh/demo/encrypt.html": ["1234"],
+    encrypt: {
+        config: {
+            "/demo/encrypt.html": ["1234"],
+            "/zh/demo/encrypt.html": ["1234"],
+        },
     },
-  },
 
-  plugins: {
+    plugins: {
 //    comment: {
 //      // @ts-expect-error: You should generate and use your own comment service
 //      provider: "Waline",
@@ -73,106 +77,106 @@ export default hopeTheme({
 //      fancy: true,
 //      duration: 1000,
 //    },
-    copyCode: false,
+        copyCode: false,
 
-    // all features are enabled for demo, only preserve features you need here
-    mdEnhance: {
-      align: true,
-      attrs: true,
-      chart: true,
-      codetabs: true,
-      demo: true,
-      echarts: true,
-      figure: true,
-      flowchart: true,
-      gfm: true,
-      imgLazyload: true,
-      imgSize: true,
-      include: true,
-      katex: true,
-      mark: true,
-      mermaid: true,
-      playground: {
-        presets: ["ts", "vue"],
-      },
-      presentation: {
-        plugins: ["highlight", "math", "search", "notes", "zoom"],
-      },
-      stylize: [
-        {
-          matcher: "Recommended",
-          replacer: ({ tag }) => {
-            if (tag === "em")
-              return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
-              };
-          },
+        // all features are enabled for demo, only preserve features you need here
+        mdEnhance: {
+            align: true,
+            attrs: true,
+            chart: true,
+            codetabs: true,
+            demo: true,
+            echarts: true,
+            figure: true,
+            flowchart: true,
+            gfm: true,
+            imgLazyload: true,
+            imgSize: true,
+            include: true,
+            katex: true,
+            mark: true,
+            mermaid: true,
+            playground: {
+                presets: ["ts", "vue"],
+            },
+            presentation: {
+                plugins: ["highlight", "math", "search", "notes", "zoom"],
+            },
+            stylize: [
+                {
+                    matcher: "Recommended",
+                    replacer: ({tag}) => {
+                        if (tag === "em")
+                            return {
+                                tag: "Badge",
+                                attrs: {type: "tip"},
+                                content: "Recommended",
+                            };
+                    },
+                },
+            ],
+            sub: true,
+            sup: true,
+            tabs: true,
+            vPre: true,
+            vuePlayground: true,
         },
-      ],
-      sub: true,
-      sup: true,
-      tabs: true,
-      vPre: true,
-      vuePlayground: true,
-    },
 
-    // uncomment these if you want a pwa
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: true,
-    //   cachePic: true,
-    //   appendBase: true,
-    //   apple: {
-    //     icon: "/assets/icon/apple-icon-152.png",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/assets/icon/ms-icon-144.png",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/icon/chrome-mask-512.png",
-    //         sizes: "512x512",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-mask-192.png",
-    //         sizes: "192x192",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     shortcuts: [
-    //       {
-    //         name: "Demo",
-    //         short_name: "Demo",
-    //         url: "/demo/",
-    //         icons: [
-    //           {
-    //             src: "/assets/icon/guide-maskable.png",
-    //             sizes: "192x192",
-    //             purpose: "maskable",
-    //             type: "image/png",
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
-  },
+        // uncomment these if you want a pwa
+        // pwa: {
+        //   favicon: "/favicon.ico",
+        //   cacheHTML: true,
+        //   cachePic: true,
+        //   appendBase: true,
+        //   apple: {
+        //     icon: "/assets/icon/apple-icon-152.png",
+        //     statusBarColor: "black",
+        //   },
+        //   msTile: {
+        //     image: "/assets/icon/ms-icon-144.png",
+        //     color: "#ffffff",
+        //   },
+        //   manifest: {
+        //     icons: [
+        //       {
+        //         src: "/assets/icon/chrome-mask-512.png",
+        //         sizes: "512x512",
+        //         purpose: "maskable",
+        //         type: "image/png",
+        //       },
+        //       {
+        //         src: "/assets/icon/chrome-mask-192.png",
+        //         sizes: "192x192",
+        //         purpose: "maskable",
+        //         type: "image/png",
+        //       },
+        //       {
+        //         src: "/assets/icon/chrome-512.png",
+        //         sizes: "512x512",
+        //         type: "image/png",
+        //       },
+        //       {
+        //         src: "/assets/icon/chrome-192.png",
+        //         sizes: "192x192",
+        //         type: "image/png",
+        //       },
+        //     ],
+        //     shortcuts: [
+        //       {
+        //         name: "Demo",
+        //         short_name: "Demo",
+        //         url: "/demo/",
+        //         icons: [
+        //           {
+        //             src: "/assets/icon/guide-maskable.png",
+        //             sizes: "192x192",
+        //             purpose: "maskable",
+        //             type: "image/png",
+        //           },
+        //         ],
+        //       },
+        //     ],
+        //   },
+        // },
+    },
 });
