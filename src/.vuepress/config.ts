fe12f3b56e218056a61f9d6a5a9991853385442b
shilinlee <836160610@qq.com>
2023-04-28 15:43:59 +0800

    fix: fix docs
    Signed-off-by: shilinlee <836160610@qq.com>

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import {searchProPlugin} from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-    base: '/docs/',
+    base: '/',
 
     locales: {
         '/': {
