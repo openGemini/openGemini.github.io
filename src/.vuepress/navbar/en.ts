566aa9f433af062668c777544c9b73489030630e
shilinlee <836160610@qq.com>
2023-05-24 00:39:01 +0800

    docs: init dev guide entrypoint (#28)
    Signed-off-by: shilinlee <836160610@qq.com>

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -2,7 +2,7 @@ import { navbar } from "vuepress-theme-hope";
 
 export const enNavbar = navbar([
   { text: "Guide", icon: "creative", link: "/guide" },
-  { text: "Development", icon: "code", link: "/dev-guide/" },
+  { text: "Development", icon: "code", link: "/dev-guide/get_started/build_source_code" },
 //  {
 //    text: "Guide",
 //    icon: "creative",
